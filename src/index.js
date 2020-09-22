f548c79a5ccba06cae9a5f8f6d7025542cba1d4b
Adam Zientarski <adam@eosdetroit.io>
2020-09-22 15:47:56 -0400

    Index.js UAL Code

--- src/index.js
+++ src/index.js
@@ -4,9 +4,34 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Anchor } from 'ual-anchor'
+import { Scatter } from 'ual-scatter'
+import { Wax } from '@eosdacio/ual-wax'
+
+import { JsonRpc } from 'eosjs'
+import { UALProvider, withUAL } from 'ual-reactjs-renderer'
+
+const waxChain = {
+  chainId: process.env.REACT_APP_WAX_CHAINID,
+  rpcEndpoints: [{
+    protocol: process.env.REACT_APP_WAX_PROTOCOL,
+    host: process.env.REACT_APP_WAX_HOST,
+    port: process.env.REACT_APP_WAX_PORT,
+  }]
+}
+ 
+const AppWithUAL = withUAL(App)
+
+const appName = process.env.REACT_APP_NAME
+const scatter = new Scatter([waxChain], { appName: appName });
+const anchor = new Anchor([waxChain], { appName: appName });
+const waxcloud = new Wax([waxChain], { appName: appName });
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UALProvider chains={[waxChain]} authenticators={[waxcloud, anchor, scatter ]} appName={appName}>
+      <AppwWithUAL />
+    </UALProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );

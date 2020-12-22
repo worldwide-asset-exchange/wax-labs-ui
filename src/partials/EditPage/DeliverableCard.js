0315a2d66c59fe3c112a01aaac9ade25d468ece0
Jose Schwengber <jose@eosdetroit.io>
2020-12-22 11:13:43 -0300

    DeliverableCards refactor

--- src/partials/EditPage/DeliverableCard.js
+++ src/partials/EditPage/DeliverableCard.js
@@ -30,30 +30,14 @@ export const RenderDeliverableCard = ({
             }
             const dragIndex = item.index;
             const hoverIndex = index;
+
             // Don't replace items with themselves
             if (dragIndex === hoverIndex) {
                 return;
             }
-            // Determine rectangle on screen
-            // const hoverBoundingRect = ref.current?.getBoundingClientRect();
-            // Get vertical middle
-            // const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
-            // Determine mouse position
-            // const clientOffset = monitor.getClientOffset();
-            // Get pixels to the top
-            // const hoverClientY = clientOffset.y - hoverBoundingRect.top;
-            // Only perform the move when the mouse has crossed half of the items height
-            // When dragging downwards, only move when the cursor is below 50%
-            // When dragging upwards, only move when the cursor is above 50%
-            // Dragging downwards
-            // if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-            //     return;
-            // }
-            // // Dragging upwards
-            // if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-            //     return;
-            // }
-            // Time to actually perform the action
+            
+            // Time to perform the action
+            // moveCard is a function passed through props.
             moveCard(dragIndex, hoverIndex);
             // Note: we're mutating the monitor item here!
             // Generally it's better to avoid mutations,
@@ -124,7 +108,7 @@ export const RenderDeliverableCard = ({
                     {recipientErrorMessage}
                 </div> 
             </div>
-            <div>{index + 1}) {(deliverable.requested_amount ? deliverable.requested_amount.toFixed(8) : "0.00000000") + " WAX"}</div>
+            <div>{index + 1}) {(deliverable.requested_amount ? deliverable.requested_amount.toFixed(8) : Number(0).toFixed(8)) + " WAX"}</div>
             <button 
                 className='btn' 
                 onClick={()=>removeCard(index)}

6fa26a0be62ba67032292a17359ee58435926494
Jose Schwengber <jose@eosdetroit.io>
2020-12-31 16:01:07 -0300

    Made notifications into a drop down

--- src/partials/Header/Notifications.js
+++ src/partials/Header/Notifications.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import DropDown from 'react-bootstrap/Dropdown'
 
 export default function RenderNotifications (props){
     return (
         <div>
-            {props.notifications.length}
-            Notifications
+            <DropDown>
+                <DropDown.Toggle variant="success" id="dropdown-basic">
+                    {props.querying ? "Loading " : props.notifications.length + " "}
+                    Notifications
+                </DropDown.Toggle>
+                <DropDown.Menu>
+                    {props.notifications.map( notification => {
+                        return (
+                            <DropDown.Item 
+                                href={"proposals/" + notification.id}
+                            >
+                                {notification.text}
+                            </DropDown.Item>
+                        )
+                    })}
+                </DropDown.Menu>
+            </DropDown>
+
         </div>
     )
 }

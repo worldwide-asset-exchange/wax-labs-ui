6910f0fd5acf9f278d8606783db3ff766fefdf9e
Karyne Mayer <mayer.karyne@gmail.com>
2021-01-08 16:15:29 -0300

    Adds assets for the header

---
+++ src/icons/RingingBellIcon.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18 13v-3a6 6 0 00-5-6V3a1 1 0 00-2 0v1a6 6 0 00-5 6v3a3 3 0 00-2 3v2a1 1 0 001 1h3a4 4 0 008 0h3a1 1 0 001-1v-2a3 3 0 00-2-3zM8 10a4 4 0 018 0v3H8v-3zm4 10a2 2 0 01-2-1h4a2 2 0 01-2 1zm6-3H6v-1a1 1 0 011-1h10a1 1 0 011 1v1z" fill-opacity=".7"/>
+        <path d="M23 4a3 3 0 11-7 0 3 3 0 017 0z" fill="#F78E1E"/>
+    </svg>
+)

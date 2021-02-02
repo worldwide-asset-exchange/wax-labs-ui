5310ab4cdc6e56aedeabce28b7d6066b204cbf4c
Karyne Mayer <mayer.karyne@gmail.com>
2021-02-02 13:05:49 -0300

    Adds roadmap to create proposal

--- src/partials/EditPage/ProposalInputContainer.js
+++ src/partials/EditPage/ProposalInputContainer.js
@@ -18,6 +18,7 @@ export default function RenderProposalInputContainer ({proposal, deprecatedCateg
         image_url:"",
         estimated_time:"",
         category: "",
+        road_map: "",
     });
 
     const [availableCategories, setAvailableCategories] = useState([]);
@@ -35,7 +36,6 @@ export default function RenderProposalInputContainer ({proposal, deprecatedCateg
 
     }
 
-    
     useEffect(()=>{
         function filterDeprecated(category) {
             return !deprecatedCategories.includes(category);
@@ -48,7 +48,7 @@ export default function RenderProposalInputContainer ({proposal, deprecatedCateg
 
     useEffect(()=>{
         updateValidatorData(validator.allValid());
-        
+
         // eslint-disable-next-line
     }, [editableProposal, totalRequested])
 
@@ -72,7 +72,7 @@ export default function RenderProposalInputContainer ({proposal, deprecatedCateg
     useEffect(()=>{
         let totalRequested = totalRequestedFunds.toFixed(8) + " WAX"
         setTotalRequested(totalRequested);
-        
+
         // Some components don't pass this callback, so only use it if it was passed.
         if(updateTotalRequestedErrorMessage){
             updateTotalRequestedErrorMessage(validator.message('total requested', totalRequestedFunds, `max:${GLOBAL_VARS.PROPOSAL_MAX_REQUESTED},num|min:${GLOBAL_VARS.PROPOSAL_MIN_REQUESTED},num`));
@@ -95,108 +95,158 @@ export default function RenderProposalInputContainer ({proposal, deprecatedCateg
     const contentErrorMessage = validator.message('content', editableProposal.content, `required|max:${GLOBAL_VARS.MAX_BODY_LENGTH}`);
     const categoryErrorMessage = validator.message('category', editableProposal.category, "required");
     const estimatedTimeErrorMessage = validator.message('estimated time', editableProposal.estimated_time, "required|min:1,num");
-    
-    return(
+    const roadmapErrorMessage = validator.message('road map', editableProposal.road_map, "required");
+
+    return (
         <div className="proposalInputContainer">
             <div className="proposalInputContainer__fieldset">
-                <label className="input__label">Title
-                    <span className={`proposalInputContainer__charAmount ${editableProposal.title.length > GLOBAL_VARS.MAX_TITLE_LENGTH ? "proposalInputContainer__charAmount--error" : ""}`}> {editableProposal.title.length}/{GLOBAL_VARS.MAX_TITLE_LENGTH}
+                <label className="input__label">
+                    Title
+                    <span
+                        className={`proposalInputContainer__charAmount ${
+                            editableProposal.title.length > GLOBAL_VARS.MAX_TITLE_LENGTH
+                                ? 'proposalInputContainer__charAmount--error'
+                                : ''
+                        }`}
+                    >
+                        {' '}
+                        {editableProposal.title.length}/{GLOBAL_VARS.MAX_TITLE_LENGTH}
                     </span>
                 </label>
                 <input
-                    className={`${titleErrorMessage ? "input input--error" : "input"}`}
+                    className={`${titleErrorMessage ? 'input input--error' : 'input'}`}
                     value={editableProposal.title}
                     name="title"
                     onChange={handleInputChange}
                 />
-                <div className="input__errorMessage">
-                    {titleErrorMessage}
-                </div>
+                <div className="input__errorMessage">{titleErrorMessage}</div>
             </div>
             <div className="proposalInputContainer__fieldset">
-                <label className="input__label">Description
-                    <span className={`proposalInputContainer__charAmount ${editableProposal.description.length > GLOBAL_VARS.MAX_DESCRIPTION_LENGTH ? "proposalInputContainer__charAmount--error" : ""}`}> {editableProposal.description.length}/{GLOBAL_VARS.MAX_DESCRIPTION_LENGTH}
+                <label className="input__label">
+                    Description
+                    <span
+                        className={`proposalInputContainer__charAmount ${
+                            editableProposal.description.length > GLOBAL_VARS.MAX_DESCRIPTION_LENGTH
+                                ? 'proposalInputContainer__charAmount--error'
+                                : ''
+                        }`}
+                    >
+                        {' '}
+                        {editableProposal.description.length}/{GLOBAL_VARS.MAX_DESCRIPTION_LENGTH}
                     </span>
                 </label>
                 <input
-                    className={`${descriptionErrorMessage ? "input input--error" : "input"}`}
+                    className={`${descriptionErrorMessage ? 'input input--error' : 'input'}`}
                     value={editableProposal.description}
                     name="description"
                     onChange={handleInputChange}
                 />
-                <div className="input__errorMessage">
-                    {descriptionErrorMessage}
-                </div>
+                <div className="input__errorMessage">{descriptionErrorMessage}</div>
             </div>
             <div className="proposalInputContainer__fieldset">
                 <label className="input__label">
                     Image URL
-                    <span className={`proposalInputContainer__charAmount ${editableProposal.image_url.length > GLOBAL_VARS.MAX_IMGURL_LENGTH ? "proposalInputContainer__charAmount--error" : ""}`}> {editableProposal.image_url.length}/{GLOBAL_VARS.MAX_IMGURL_LENGTH}
+                    <span
+                        className={`proposalInputContainer__charAmount ${
+                            editableProposal.image_url.length > GLOBAL_VARS.MAX_IMGURL_LENGTH
+                                ? 'proposalInputContainer__charAmount--error'
+                                : ''
+                        }`}
+                    >
+                        {' '}
+                        {editableProposal.image_url.length}/{GLOBAL_VARS.MAX_IMGURL_LENGTH}
                     </span>
                 </label>
                 <input
-                    className={`${imageUrlErrorMessage ? "input input--error" : "input"}`}
+                    className={`${imageUrlErrorMessage ? 'input input--error' : 'input'}`}
                     value={editableProposal.image_url}
                     name="image_url"
                     onChange={handleInputChange}
                 />
-                <div className="input__errorMessage">
-                    {imageUrlErrorMessage}
-                </div>
+                <div className="input__errorMessage">{imageUrlErrorMessage}</div>
             </div>
             <div className="proposalInputContainer__fieldset">
-                <label className="input__label">Content
-                    <span className={`proposalInputContainer__charAmount ${editableProposal.content.length > GLOBAL_VARS.MAX_BODY_LENGTH ? "proposalInputContainer__charAmount--error" : ""}`}> {editableProposal.content.length}/{GLOBAL_VARS.MAX_BODY_LENGTH}
+                <label className="input__label">
+                    Content
+                    <span
+                        className={`proposalInputContainer__charAmount ${
+                            editableProposal.content.length > GLOBAL_VARS.MAX_BODY_LENGTH
+                                ? 'proposalInputContainer__charAmount--error'
+                                : ''
+                        }`}
+                    >
+                        {' '}
+                        {editableProposal.content.length}/{GLOBAL_VARS.MAX_BODY_LENGTH}
                     </span>
                 </label>
                 <textarea
-                    className={`${contentErrorMessage ? "textarea textarea--error" : "textarea"}`}
+                    className={`${contentErrorMessage ? 'textarea textarea--error' : 'textarea'}`}
                     value={editableProposal.content}
                     name="content"
                     onChange={handleInputChange}
                 />
-                <div className="input__errorMessage">
-                    {contentErrorMessage}
-                </div>
+                <div className="input__errorMessage">{contentErrorMessage}</div>
+            </div>
+            <div className="proposalInputContainer__fieldset">
+                <label className="input__label">
+                    Roadmap
+                    <span
+                        className={`proposalInputContainer__charAmount ${
+                            editableProposal.road_map.length > GLOBAL_VARS.MAX_ROADMAP_LENGTH
+                                ? 'proposalInputContainer__charAmount--error'
+                                : ''
+                        }`}
+                    >
+                        {' '}
+                        {editableProposal.road_map.length}/{GLOBAL_VARS.MAX_ROADMAP_LENGTH}
+                    </span>
+                </label>
+                <input
+                    className={`${roadmapErrorMessage ? 'input input--error' : 'input'}`}
+                    value={editableProposal.road_map}
+                    name="road_map"
+                    onChange={handleInputChange}
+                />
+                <div className="input__errorMessage">{roadmapErrorMessage}</div>
             </div>
             <div className="proposalInputContainer__fieldset">
                 <label className="input__label">Estimated time (days)</label>
                 <input
-                    className={`${estimatedTimeErrorMessage ? "input input--error" : "input"}`}
+                    className={`${estimatedTimeErrorMessage ? 'input input--error' : 'input'}`}
                     type="number"
                     value={editableProposal.estimated_time}
                     name="estimated_time"
                     onChange={handleInputChange}
                 />
-                <div className="input__errorMessage">
-                    {estimatedTimeErrorMessage}
-                </div>
+                <div className="input__errorMessage">{estimatedTimeErrorMessage}</div>
             </div>
             <div className="proposalInputContainer__fieldset">
                 <label className="input__label">Category</label>
                 <select
-                    className={`${categoryErrorMessage ? "select select--error" : "select"}`}
+                    className={`${categoryErrorMessage ? 'select select--error' : 'select'}`}
                     value={categories[editableProposal.category]}
                     name="category"
                     onChange={handleInputChange}
                 >
-                    <option value={""}></option>
-                    {availableCategories.map((category) =>{
-                        return <option className="select__option" key={category} value={category}>{category}</option>
+                    <option value={''}></option>
+                    {availableCategories.map((category) => {
+                        return (
+                            <option className="select__option" key={category} value={category}>
+                                {category}
+                            </option>
+                        );
                     })}
                 </select>
-                <div className="input__errorMessage">
-                    {categoryErrorMessage}
-                </div>
+                <div className="input__errorMessage">{categoryErrorMessage}</div>
             </div>
-            {
-                !hideTotalRequested ?
+            {!hideTotalRequested ? (
                 <div className="proposalInputContainer__fieldset">
                     <div className="input__label">Total amount requested</div>
                     <h4>{totalRequested}</h4>
                 </div>
-                : ""
-            }
+            ) : (
+                ''
+            )}
         </div>
-    )
+    );
 }

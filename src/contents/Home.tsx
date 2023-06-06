f189aa277257acd56e137b7d5ff7e55067d5d8db
Alexandre Santos <alexandressjr@gmail.com>
2023-06-06 18:39:43 -0300

    Enhancing mobile responsivity

--- src/contents/Home.tsx
+++ src/contents/Home.tsx
@@ -27,7 +27,7 @@ export const Home: React.FC = () => {
       <div className="m-8">
         <Button variant="primary">{strings.seeProposals}</Button>
       </div>
-      <div className="my-40 grid grid-cols-2 gap-1 text-start">
+      <div className="my-40 grid grid-cols-1 gap-1 text-start md:grid-cols-2">
         <div className="grid grid-rows-2 gap-1">
           <div className="grid gap-1">
             <div className="title-2 rounded-xl bg-subtle p-12">{strings.homeDescription}</div>
@@ -48,7 +48,7 @@ export const Home: React.FC = () => {
           </div>
         </div>
         <div className="grid grid-cols-2 gap-1">
-          <div className="flex flex-col justify-between rounded-xl bg-[#212C59] p-12 text-[#899CF8]">
+          <div className="flex flex-col justify-between gap-2 rounded-xl bg-[#212C59] p-12 text-[#899CF8]">
             <div>
               <div className="display-1">{stats.inReview}</div>
               <div className="label-2">{strings.inReview}</div>
@@ -59,7 +59,7 @@ export const Home: React.FC = () => {
               </Button>
             </div>
           </div>
-          <div className="flex flex-col justify-between rounded-xl bg-[#4A250D] p-12 text-[#F09150]">
+          <div className="flex flex-col justify-between gap-2 rounded-xl bg-[#4A250D] p-12 text-[#F09150]">
             <div>
               <div className="display-1">{stats.inVoting}</div>
               <div className="label-2">{strings.inVoting}</div>
@@ -70,7 +70,7 @@ export const Home: React.FC = () => {
               </Button>
             </div>
           </div>
-          <div className="flex flex-col justify-between rounded-xl bg-[#3F2353] p-12 text-[#B57DE9]">
+          <div className="flex flex-col justify-between gap-2 rounded-xl bg-[#3F2353] p-12 text-[#B57DE9]">
             <div>
               <div className="display-1">{stats.inProgress}</div>
               <div className="label-2">{strings.inProgress}</div>
@@ -81,7 +81,7 @@ export const Home: React.FC = () => {
               </Button>
             </div>
           </div>
-          <div className="flex flex-col justify-between rounded-xl bg-[#213824] p-12 text-[#7BBF7C]">
+          <div className="flex flex-col justify-between gap-2 rounded-xl bg-[#213824] p-12 text-[#7BBF7C]">
             <div>
               <div className="display-1">{stats.completed}</div>
               <div className="label-2">{strings.completed}</div>

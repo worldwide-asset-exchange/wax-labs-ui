dea9c5935dea2ea6417973f396ee16fe99809165
Bruno Souza <brnosouza@gmail.com>
2023-06-27 10:30:11 -0300

    WL-16: Applying code review

--- src/hooks/useNotifications.ts
+++ src/hooks/useNotifications.ts
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useInterval } from 'usehooks-ts';
+import { useQuery } from '@tanstack/react-query';
+import { useCallback, useState } from 'react';
 
 import adminEndVotingNotifications from '@/api/chain/notifications/adminEndVotingNotifications.ts';
 import adminToReviewNotifications from '@/api/chain/notifications/adminToReviewNotifications.ts';
@@ -11,57 +11,60 @@ import { WaxLabsNotification } from '@/api/models/notifications.ts';
 
 const READ_NOTIFICATIONS_STORAGE = 'waxlabs:readNotifications';
 
+const setStorage = (readNotifications: string[]) => {
+  localStorage.setItem(READ_NOTIFICATIONS_STORAGE, JSON.stringify(readNotifications));
+};
+
+const getStorage = (): string[] => {
+  return JSON.parse(localStorage.getItem(READ_NOTIFICATIONS_STORAGE) || '[]');
+};
+
+const appendAndSetStorage = (notificationKey: string) => {
+  const notifications = getStorage();
+  notifications.push(notificationKey);
+  setStorage(notifications);
+  return notifications;
+};
+
 export function useNotifications({ accountName, isAdmin = false }: { accountName: string; isAdmin?: boolean }): {
   notifications: WaxLabsNotification[];
   readNotification: (notification: WaxLabsNotification) => void;
 } {
-  const [notifications, setNotifications] = useState<WaxLabsNotification[]>([]);
-  const [readNotifications, setReadNotifications] = useState<string[]>(
-    JSON.parse(localStorage.getItem(READ_NOTIFICATIONS_STORAGE) || '[]')
-  );
+  const [read, setRead] = useState<string[]>(getStorage());
 
-  const fetchNotifications: () => Promise<WaxLabsNotification[]> = useCallback(() => {
-    const promises = [
-      proposerEndVotingNotifications({ accountName }),
-      startVotingNotifications({ accountName }),
-      proposerDeliverableNotifications({ accountName }),
-      reviewerDeliverableNotifications({ accountName }),
-    ];
+  const { data: notificationResult } = useQuery<WaxLabsNotification[]>({
+    queryFn: () => {
+      const promises = [
+        proposerEndVotingNotifications({ accountName }),
+        startVotingNotifications({ accountName }),
+        proposerDeliverableNotifications({ accountName }),
+        reviewerDeliverableNotifications({ accountName }),
+      ];
 
-    if (isAdmin) {
-      promises.push(adminEndVotingNotifications(), adminToReviewNotifications());
+      if (isAdmin) {
+        promises.push(adminEndVotingNotifications(), adminToReviewNotifications());
+      }
+
+      return Promise.all(promises).then(notifications => notifications.flat());
+    },
+    queryKey: [accountName, isAdmin],
+    refetchInterval: 10e4,
+  });
+
+  const readNotification = useCallback((notification: WaxLabsNotification) => {
+    const readNotifications = getStorage();
+
+    if (!readNotifications.includes(notification.readNotificationKey)) {
+      setRead(appendAndSetStorage(notification.readNotificationKey));
     }
-
-    return Promise.all(promises).then(notifications =>
-      notifications.flat().filter(n => !readNotifications.includes(n.readNotificationKey))
-    );
-  }, [accountName, isAdmin, readNotifications]);
-
-  useInterval(() => {
-    fetchNotifications().then(notifications => {
-      setNotifications(notifications);
-    });
-  }, 10e3);
-
-  useEffect(() => {
-    fetchNotifications().then(notifications => {
-      setNotifications(notifications);
-    });
   }, []);
 
-  const readNotification = useCallback(
-    (notification: WaxLabsNotification) => {
-      if (!readNotifications.includes(notification.readNotificationKey)) {
-        readNotifications.push(notification.readNotificationKey);
-        localStorage.setItem(READ_NOTIFICATIONS_STORAGE, JSON.stringify(readNotifications));
-        setReadNotifications(readNotifications);
-      }
-    },
-    [readNotifications]
-  );
-
   return {
-    notifications,
+    notifications:
+      notificationResult?.filter(n => {
+        const filterReadNotifications = read.length ? read : getStorage();
+        return !filterReadNotifications.includes(n.readNotificationKey);
+      }) ?? [],
     readNotification,
   };
 }

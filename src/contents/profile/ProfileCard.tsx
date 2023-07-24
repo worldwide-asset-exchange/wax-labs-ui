332c26133b8fe929f743d8aea0c75e63377f12d0
Elton Pongilo <elton.pongilo@gmail.com>
2023-07-24 17:32:53 -0300

    Changin article to section

--- src/contents/profile/ProfileCard.tsx
+++ src/contents/profile/ProfileCard.tsx
@@ -31,12 +31,12 @@ export function ProfileCard({
   const { t } = useTranslation();
 
   return (
-    <article className="space-y-8 rounded-xl bg-subtle p-8">
+    <section className="space-y-8 rounded-xl bg-subtle p-8">
       <header className="flex flex-wrap items-center gap-4">
         <div className="flex flex-1 items-center gap-4">
           <div className="flex h-14 min-h-[56px] w-14 min-w-[56px] flex-none items-center justify-center rounded-full border-2">
             {imageUrl ? (
-              <img className="h-full w-full rounded-full object-cover" src={imageUrl} />
+              <img className="h-full w-full rounded-full object-cover" src={imageUrl} alt="" />
             ) : (
               <MdPerson className="text-low-contrast" size={28} />
             )}
@@ -48,10 +48,10 @@ export function ProfileCard({
         </div>
         {children && <div className="flex-none">{children}</div>}
       </header>
-      <section className="space-y-1">
+      <div className="space-y-1">
         <h3 className="label-2 text-high-contrast">{t('biography')}</h3>
         <p className="body-1 text-low-contrast">{biography}</p>
-      </section>
+      </div>
       <Info.Root>
         <Info.Item label={t('groupName')} value={groupName} className="border-t border-subtle-light">
           <MdOutlineGroups size={24} />
@@ -66,6 +66,6 @@ export function ProfileCard({
           <FaTelegramPlane size={24} />
         </Info.Item>
       </Info.Root>
-    </article>
+    </section>
   );
 }

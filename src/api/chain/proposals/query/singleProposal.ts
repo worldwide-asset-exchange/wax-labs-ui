6394abe3f07ce62f52b26ab327c02c05759e4df5
Bruno Souza <brnosouza@gmail.com>
2023-08-08 16:35:53 -0300

    We should return null if the proposal is not found

--- src/api/chain/proposals/query/singleProposal.ts
+++ src/api/chain/proposals/query/singleProposal.ts
@@ -3,11 +3,15 @@ import { UInt64 } from '@wharfkit/antelope';
 import { getProposals } from '@/api/chain/proposals/query/getProposals.ts';
 import { Proposal } from '@/api/models/proposal.ts';
 
-export async function singleProposal({ proposalId }: { proposalId: number }): Promise<Proposal> {
-  const proposals = await getProposals({
-    lowerBound: UInt64.from(proposalId),
-    upperBound: UInt64.from(proposalId),
-  });
+export async function singleProposal({ proposalId }: { proposalId: number }): Promise<Proposal | null> {
+  try {
+    const proposals = await getProposals({
+      lowerBound: UInt64.from(proposalId),
+      upperBound: UInt64.from(proposalId),
+    });
 
-  return proposals?.[0];
+    return proposals?.[0] ?? null;
+  } catch {
+    return null;
+  }
 }

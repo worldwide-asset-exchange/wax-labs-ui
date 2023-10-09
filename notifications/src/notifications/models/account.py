25f239e6bba664e7d907becf6e9e22550249f01e
Bruno Souza <brnosouza@gmail.com>
2023-10-09 17:39:16 -0300

    Chat ID shouldn't be required if the user was added by the CLI

--- notifications/src/notifications/models/account.py
+++ notifications/src/notifications/models/account.py
@@ -16,7 +16,7 @@ class User(BaseModel):
     name: Mapped[typing.Optional[str]] = mapped_column(String(160), nullable=True)
     telegram_account: Mapped[str] = mapped_column(String(160), index=True, nullable=False)
     wax_account: Mapped[str] = mapped_column(String(15), unique=True, nullable=False)
-    chat_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    chat_id: Mapped[typing.Optional[int]] = mapped_column(unique=True, nullable=True)
     deleted_at: Mapped[typing.Optional[datetime]] = mapped_column(nullable=True)
 
     subscriptions: Mapped["Subscription"] = relationship(back_populates="user", lazy="noload", uselist=True)

1de5962dbe97ce68a5c703171d96584f32ccfe3c
Bruno Souza <brnosouza@gmail.com>
2023-10-10 19:25:51 -0300

    All commands require a proposal id to be sent

--- notifications/src/notifications/bot/bot.py
+++ notifications/src/notifications/bot/bot.py
@@ -8,12 +8,10 @@ logger = logging.getLogger("waxlabs")
 COMMANDS = [
     BotCommand(command="start", description="Start the bot."),
     BotCommand(command="cancel", description="Cancel the current operation."),
-    BotCommand(command="stop", description="Stop the bot."),
+    BotCommand(command="stop", description="Remove all your subscriptions."),
     BotCommand(
         command="status",
-        description=(
-            "Get the status of all proposals. You can also send the proposal id to get the status of a single proposal."
-        ),
+        description="Get the status of a single proposal. You need to send the proposal id.",
     ),
     BotCommand(command="help", description="Display the help message"),
     BotCommand(

3963edad279eaf20385a5e4abeeec97216fe3f61
includenull <15929809+includenull@users.noreply.github.com>
2026-06-11 00:21:34 +0100

    chore: switch to vite react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
